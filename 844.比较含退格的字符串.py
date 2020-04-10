f5869cfe3d718d7aaff15b28cf4efc0d60579d06
summingyu <summingbest@gmail.com>
2020-04-10 09:41:06 +0800

    commit for 20200410

---
+++ 844.比较含退格的字符串.py
@@ -0,0 +1,79 @@
+#
+# @lc app=leetcode.cn id=844 lang=python3
+#
+# [844] 比较含退格的字符串
+#
+# https://leetcode-cn.com/problems/backspace-string-compare/description/
+#
+# algorithms
+# Easy (50.15%)
+# Likes:    99
+# Dislikes: 0
+# Total Accepted:    17.6K
+# Total Submissions: 35K
+# Testcase Example:  '"ab#c"\n"ad#c"'
+#
+# 给定 S 和 T 两个字符串，当它们分别被输入到空白的文本编辑器后，判断二者是否相等，并返回结果。 # 代表退格字符。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 输入：S = "ab#c", T = "ad#c"
+# 输出：true
+# 解释：S 和 T 都会变成 “ac”。
+# 
+# 
+# 示例 2：
+# 
+# 输入：S = "ab##", T = "c#d#"
+# 输出：true
+# 解释：S 和 T 都会变成 “”。
+# 
+# 
+# 示例 3：
+# 
+# 输入：S = "a##c", T = "#a#c"
+# 输出：true
+# 解释：S 和 T 都会变成 “c”。
+# 
+# 
+# 示例 4：
+# 
+# 输入：S = "a#c", T = "b"
+# 输出：false
+# 解释：S 会变成 “c”，但 T 仍然是 “b”。
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 <= S.length <= 200
+# 1 <= T.length <= 200
+# S 和 T 只含有小写字母以及字符 '#'。
+# 
+# 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        queueS = []
+        queueT = []
+        for i in S:
+            if i == "#" and queueS:
+                queueS.pop()
+            elif i != "#":
+                queueS.append(i)
+        for j in T:
+            if j == "#" and queueT:
+                queueT.pop()
+            elif j != "#":
+                queueT.append(j)
+        return queueS == queueT
+        
+# @lc code=end
+
